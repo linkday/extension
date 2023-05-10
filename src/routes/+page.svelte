8fa600496483814fd77af27363ed0ed1e644b14e
Xyphuz <hansen0715.2@gmail.com>
2023-05-10 17:58:14 +0800

    chore: update submit ux

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,15 @@
 	import { browser } from "$app/environment";
 	import { ExtensionMode } from "../models/extension-mode";
 	import { env } from "$env/dynamic/public";
+	import { tweened } from "svelte/motion";
+	import { circIn } from "svelte/easing";
 
 	export let data;
+
+	function getExtensionMode(): ExtensionMode {
+		return data.mode;
+	}
+	$: mode = getExtensionMode();
 
 	const { form, errors, constraints } = superForm(data.form);
 
@@ -121,12 +128,19 @@
 			currentFocusIndex = 0;
 		}
 	}
+
+	let isSubmitting = false;
+
+	const strokeOffset = tweened(50, {
+		duration: 250,
+		easing: circIn,
+	});
 </script>
 
 <svelte:window on:click={handleClickEvent} />
 
 <div class="min-w-[360px]">
-	{#if data.mode === ExtensionMode.ShowForm}
+	{#if mode === ExtensionMode.ShowForm}
 		<div class="mb-4 flex flex-col p-8">
 			<div class="mb-12 text-4xl font-bold">Add Bookmark</div>
 			<div class="grid grid-flow-row gap-4">
@@ -245,6 +259,8 @@
 								return;
 							}
 
+							isSubmitting = true;
+
 							api
 								.addBookmark($form, {
 									withCredentials: true,
@@ -252,19 +268,41 @@
 								.then((resp) => {
 									console.log(resp);
 									// goto("/bookmarks");
+									mode = ExtensionMode.ShowFeed;
+									strokeOffset.set(25);
 								})
 								.catch((err) => {
 									console.log(err);
+								})
+								.finally(() => {
+									isSubmitting = false;
 								});
 						}}
-						class=" w-full rounded-lg bg-main px-4 py-2 text-sm font-medium text-white hover:bg-hover focus:outline-none focus:ring-4 focus:ring-blue-300"
+						class=" flex w-full items-center justify-center rounded-lg bg-main px-4 py-2 text-sm font-medium text-white hover:bg-hover focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:opacity-50"
+						class:disabled={isSubmitting}
 					>
+						{#if isSubmitting}
+							<svg
+								aria-hidden="true"
+								class="mr-2 h-4 w-4 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+								viewBox="0 0 100 101"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								><path
+									d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+									fill="currentColor"
+								/><path
+									d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+									fill="currentFill"
+								/></svg
+							>
+						{/if}
 						Add
 					</button>
 				</div>
 			</div>
 		</div>
-	{:else if data.mode === ExtensionMode.ShowLoginLink}
+	{:else if mode === ExtensionMode.ShowLoginLink}
 		<div class="flex h-24 w-full flex-col items-center justify-center">
 			<button
 				class="rounded-lg bg-main px-4 py-2 text-sm font-bold text-white hover:bg-hover focus:outline-none focus:ring-4 focus:ring-blue-300"
@@ -274,6 +312,26 @@
 			>
 				Login to add bookmarks
 			</button>
+		</div>
+	{:else if mode === ExtensionMode.ShowFeed}
+		<div class="my-2 flex h-24 w-full flex-col items-center justify-center">
+			<div class="flex flex-col items-center justify-center">
+				<svg
+					class="h-12 w-12 text-green-500"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					style="stroke-dashoffset: {$strokeOffset}; stroke-dasharray: 50"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M5 13l4 4L19 7"
+					/>
+				</svg>
+				<div class="text-center text-gray-600 dark:text-gray-400">Bookmark added successfully.</div>
+			</div>
 		</div>
 	{/if}
 </div>
